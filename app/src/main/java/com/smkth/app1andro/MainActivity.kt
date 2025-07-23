a5ecb5cd828af26b5680d67832a22afd1cdf0951
Androw <thatsdiamonds@gmail.com>
2025-07-23 12:49:54 +0700

    first commit

---
+++ app/src/main/java/com/smkth/app1andro/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.smkth.app1andro
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import com.smkth.app1andro.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Tombol Instagran
+        binding.btnInstagram.setOnClickListener {
+            val instagramIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://instagram.com/"))
+            startActivity(instagramIntent)
+        }
+
+        // Tombol Whatasapp
+        binding.btnWhatsapp.setOnClickListener {
+            val phoneNumber = "0828"
+            val whatsappIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://wa.me/$phoneNumber"))
+            startActivity(whatsappIntent)
+        }
+    }
+}
